b172cea91a5c96821cf68c07dee3b05eab3c7f3f
mreliptik <victormeunier.dev@gmail.com>
2020-09-19 16:34:52 +0200

    ui scene

--- gui.gd
+++ gui.gd
@@ -1,36 +1,42 @@
 extends Control
 
+signal picked_from_inventory(object)
+
 const arrow = preload("res://sprites/ui/cursor_pointer3D.png")
 const hand = preload("res://sprites/ui/cursor_hand.png")
 
+const pickable = preload("res://pickableObject.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var pickable_instance = pickable.instance()
+	Inventory.inventory.append(pickable_instance)
+	pickable_instance.init(pickable_instance.TYPE.CATAPULT)
+
+
+func _on_TextureButton_pressed():
+	if Inventory.inventory[0]:
+		print(Inventory.inventory[0])
+		emit_signal("picked_from_inventory", Inventory.inventory[0])
+
+func _on_TextureButton2_pressed():
 	pass # Replace with function body.
 
 
-func _on_Button_mouse_entered():
+func _on_TextureButton3_pressed():
+	pass # Replace with function body.
+
+
+func _on_TextureButton4_pressed():
+	pass # Replace with function body.
+
+
+func _on_TextureButton5_pressed():
+	pass # Replace with function body.
+
+
+func _on_TextureButton_mouse_entered():
 	Input.set_custom_mouse_cursor(hand)
 
-
-func _on_Button_mouse_exited():
+func _on_TextureButton_mouse_exited():
 	Input.set_custom_mouse_cursor(arrow)
-
-
-func _on_Button_pressed():
-	pass # Replace with function body.
-
-
-func _on_Button2_pressed():
-	pass # Replace with function body.
-
-
-func _on_Button3_pressed():
-	pass # Replace with function body.
-
-
-func _on_Button4_pressed():
-	pass # Replace with function body.
-
-
-func _on_Button5_pressed():
-	pass # Replace with function body.
