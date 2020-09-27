7f477ce23a3cc058c53b13d06e80ace94484a22c
mreliptik <victormeunier.dev@gmail.com>
2020-09-27 23:02:25 +0200

    add msuic & effect bus

--- scenes/ui/titlescreen.gd
+++ scenes/ui/titlescreen.gd
@@ -1,7 +1,8 @@
 extends Control
 
-const game_scene = "res://scenes/effects/game.tscn"
-#const settings_scene = preload("res://scenes/ui/settings.tscn")
+const game_scene = "res://scenes/game.tscn"
+
+const settings_scene = preload("res://scenes/ui/settings.tscn")
 const credits_scene = preload("res://scenes/ui/credits.tscn")
 
 # Called when the node enters the scene tree for the first time.
@@ -12,8 +13,7 @@ func _on_PlayBtn_pressed():
 	get_tree().change_scene(game_scene)
 
 func _on_SettingsBtn_pressed():
-	pass # Replace with function body.
-	#add_child(settings_scene.instance)
+	add_child(settings_scene.instance())
 
 func _on_CreditsBtn_pressed():
 	add_child(credits_scene.instance())
